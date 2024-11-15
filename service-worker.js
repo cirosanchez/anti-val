ba97337d3e02bf2bdef2001d598ca637a8834b40
cirosanchez <ciroandressanchezbuitrago@gmail.com>
2024-11-15 11:21:08 -0500

    modified for school use case

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,9 @@ chrome.tabs.onUpdated.addListener(NOTcheckIfValUpdated);
 
 function NOTcheckIfValUpdated(tabId, changeInfo, tab) {
     const url = changeInfo.url;
-    if (url && url.includes("playvalorant.com")) {
+    if (url && (url.includes("1v1.lol" || url.includes("1001juegos.com") || url.includes("minijuegos.com") || url.includes("cooljuegos.com") || url.includes("playvalorant.com")))) {
         chrome.tabs.remove(tab.id);
-        const newURL = "https://www.youtube.com/watch?v=WZoZWFj2BRE";
+        const newURL = "https://www.youtube.com/watch?v=XhAMZq44OTQ&t=4s";
         chrome.tabs.create({ url: newURL });
     }
 
